cdb838019e8c9a65031582b37186c3e4604ff094
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2021-05-28 12:42:02 +0900

    Object.create使用

---
+++ src/TestClass.ts
@@ -0,0 +1,7 @@
+export class TestClass {
+  constructor(private memo: string) {}
+
+  getMemo(): string {
+    return this.memo;
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
-function run() {
-  console.log(process.argv);
+import { TestClass } from './TestClass';
+
+/**
+ * Object.create使用パターン
+ */
+function run1(): void {
+  const obj = Object.create(TestClass.prototype) as TestClass;
+  console.log('run1: ' + obj.getMemo());
 }
 
-run();
+/**
+ * Object.create + constructor.apply使用パターン
+ */
+function run2(): void {
+  const obj = Object.create(TestClass.prototype) as TestClass;
+  obj.constructor.apply(obj, ['hoge']);
+  console.log('run2: ' + obj.getMemo());
+}
+
+run1();
+run2();
